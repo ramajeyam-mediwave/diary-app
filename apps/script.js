2ae1297b7c30177e1ed0831540feccdb8846c901
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-09-15 17:08:48 +0530

    Updated

--- apps/script.js
+++ apps/script.js
@@ -10,8 +10,8 @@ function makediaryDiv(diary) {
   textDiv.setAttribute("class", "textDiv");
   div.appendChild(textDiv);
   const h2 = document.createElement("h2");
-  h2.innerText = diary["diary_date"];
-  //   date.innerText = reverseDate(diary["diary_date"])
+  // h2.innerText = diary["diary_date"];
+  h2.innerText = reverseDate(diary["diary_date"])
 
   const h3 = document.createElement("h3");
   h3.innerText = diary["diary_steps"];
@@ -40,7 +40,7 @@ function removediary(diaryId) {
 
 function updatediaryListUI() {
   clearApp();
-  //sortarray();
+  sortarray();
   const diaryApp = document.querySelector("#app");
   for (let i = 0; i < favdiary.length; i++) {
     const diaryDiv = makediaryDiv(favdiary[i]);
@@ -48,19 +48,20 @@ function updatediaryListUI() {
   }
 }
 
+function sortarray() {
+  const test = favdiary.sort(function (a, b) {
+    // console.log(a, b);
+    return new Date(b.diary_date) - new Date(a.diary_date);
+  });
+  // console.log(test);
+}
+
 function adddiary(diary) {
   favdiary.push(diary);
-  sortarray();
+  // sortarray();
   updatediaryListUI();
   saveToLocalStorage();
 }
-function sortarray() {
-    favdiary.sort(function (a, b) {
-      return new Date(b.date) - new Date(a.date);
-    });
-  }
-  
-
 
 function clear() {
   document.querySelector("#form-div").reset();
@@ -105,11 +106,10 @@ function getFromLocalStorage() {
   }
 }
 
-
-//   function reverseDate(date){
-//     let d = date.split("-").reverse().join("-");
-//     return d;
-//   }
+  function reverseDate(date){
+    let d = date.split("-").reverse().join("-");
+    return d;
+  }
 
 getFromLocalStorage();
 updatediaryListUI();
